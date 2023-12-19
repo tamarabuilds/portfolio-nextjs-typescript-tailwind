9d0dc83c8e4f62d9d1888a0c93613f335a52453e
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-12-18 21:22:24 -0800

    add images

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,12 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import gameImg from "@/public/game.png";
+import nonprofitImg from "@/public/nonprofit.png";
+import schooldbImg from "@/public/schooldb.png";
+// import corpcommentImg from "@/public/corpcomment.png";
+// import rmtdevImg from "@/public/rmtdev.png";
+// import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
   {
