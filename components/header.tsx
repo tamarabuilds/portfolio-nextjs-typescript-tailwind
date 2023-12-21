e4d4195b592fc1f7d58236b7d9b5e1509ac502ff
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-12-20 20:31:46 -0800

    typo

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 export default function Header() {
     return <header className="z-[999] relative">
