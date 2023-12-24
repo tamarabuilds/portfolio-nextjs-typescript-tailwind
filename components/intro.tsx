f3eb2c90818a56211ee3813826db959e3ab28b54
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-12-23 22:22:01 -0800

    animated profile pic

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,24 +1,46 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export default function Intro() {
     return (
         <section>
             <div className="flex item-center justify-center">
                 <div className="relative">
-                    <Image 
-                        src="/tamara.jpg"
-                        alt="Tamara portrait"
-                        width="192"
-                        height="192"
-                        quality={95}
-                        priority={true}
-                        className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white- shadow-xl"
-                    />
+                    <motion.div 
+                        initial={{ opacity: 0, scale: 0 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        transition={{
+                            type: "tween",
+                            duration: 0.2,
+                        }}
+                    >
+                        <Image 
+                            src="/tamara.jpg"
+                            alt="Tamara portrait"
+                            width="192"
+                            height="192"
+                            quality={95}
+                            priority={true}
+                            className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white- shadow-xl"
+                        />
+                        
+                    </motion.div>
 
-                    <span className="absolute bottom-0 right-0 text-4xl">
+                    <motion.span className="absolute bottom-0 right-0 text-4xl"
+                        initial={{ opacity: 0, scale: 0 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        transition={{
+                            type: 'spring',
+                            stiffness: 125,
+                            delay: 0.1,
+                            duration: 0.7,
+                        }}
+                    >
                         👋
-                    </span>
+                    </motion.span>
                 </div>
             </div>
         </section>
