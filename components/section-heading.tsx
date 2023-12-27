9b122e5b38db0d4d8d88bb08427f77d954f9dad1
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-12-26 18:04:53 -0800

    project framework

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -4,7 +4,7 @@ type SectionHeadingProps = {
     children: React.ReactNode;
 }
 
-export default function SectionHeading({ children }): SectionHeadingProps {
+export default function SectionHeading({ children }: SectionHeadingProps ) {
     return (
         <h2 className="text-3xl font-medium capitalize mb-8">{children}</h2>
     )
