8e8566ab8ae8e8c3ed462942d56842ed1e592d1c
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-12-30 21:05:13 -0800

    update scale

--- components/projects.tsx
+++ components/projects.tsx
@@ -38,7 +38,7 @@ function Project({ title, description, tags, imageUrl }:
 
             <Image 
                 src={imageUrl} alt={description} quality={95} 
-                className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition group-hover:scale-105 group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:right-[initial] group-even:-left-40"/>
+                className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition group-hover:scale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:right-[initial] group-even:-left-40"/>
         </section>
     )
 }
