e2a4b852089fddf3da69f9dcba4f653ac3da5609
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2024-01-01 21:49:17 -0800

    setting up context

---
+++ context/active-section-context.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+
+export default function ActiveSectionContextProvider() {
+    const [activeSection, setActiveSection] = useState<>('Home');
+
+
+    return (
+        <div></div>
+    )
+}
