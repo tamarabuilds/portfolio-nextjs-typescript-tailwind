6da8f5fe82457d69ab5131ce0b8bc02b5d43b21a
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2024-01-07 18:34:40 -0800

    building out more context

--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -1,12 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, createContext } from "react";
 import { links } from "@/lib/data";
 
 type SectionName = (typeof links)[number]["name"]
 
-export default function ActiveSectionContextProvider({ children }: {
+type ActiveSectionContextProviderProps = {
     chilren: React.ReactNode;
+}
+
+type ActiveSectionContext = {
+    activeSection: SectionName;
+    setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>
+}
+
+// do not want to use value outside provider component so we use null for detaul context value
+const ActiveSectionContext = createContext<ActiveSectionContextType>(null);
+
+export default function ActiveSectionContextProvider({ children }: {
+    ActiveSectionContextProviderProps;
 }) {
     const [activeSection, setActiveSection] = useState<SectionName>('Home');
 
-    return children;
+    return <ActiveSectionContext.Provider value={{
+        activeSection,
+        setActiveSection
+    }}>
+        { children }
+    </ActiveSectionContext.Provider>;
 }
