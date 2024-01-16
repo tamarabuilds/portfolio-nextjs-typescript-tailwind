d5dcc4b5f885850b7b3f942ccd55e933984d5800
Tamara Patterson <tamarabpatterson@gmail.com>
2024-01-15 20:47:47 -0800

    updated active section

--- components/header.tsx
+++ components/header.tsx
@@ -5,10 +5,11 @@ import { motion } from "framer-motion";
 import { links } from "@/lib/data";
 import Link from "next/link";
 import clsx from "clsx";
-import { ActiveSectionContext } from "@/context/active-section-context";
+// import { ActiveSectionContext } from "@/context/active-section-context";
 
 export default function Header() {
-    const context = useContext(ActiveSectionContext);
+    const { activeSection } 
+    // const context = useContext(ActiveSectionContext);
 
     return <header className="z-[999] relative">
         <motion.div className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
