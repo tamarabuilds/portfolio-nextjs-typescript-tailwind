9d4ee2824b584637cae811faf41c4afbac3ae190
Tamara Patterson <tamarabpatterson@gmail.com>
2024-01-30 14:56:15 -0800

    fixed inView ref

--- components/about.tsx
+++ components/about.tsx
@@ -7,20 +7,22 @@ import { useInView } from "react-intersection-observer";
 import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function About() {
-    const { setActiveSection } = useActiveSectionContext();
+    // const { setActiveSection } = useActiveSectionContext();
     const { ref, inView } = useInView();
 
     console.log(inView)
 
-    // useEffect to keep external state of UI position with navigation
-    useEffect(() => {
-        if (inView) {
-            setActiveSection("About");
-        }        
-    }, [inView])
+    // // useEffect to keep external state of UI position with navigation
+    // useEffect(() => {
+    //     if (inView) {
+    //         setActiveSection("About");
+    //     }        
+    // }, [inView, setActiveSection])
 
     return (
-        <motion.section className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+        <motion.section
+            ref={ref}
+            className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
             initial={{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.175 }}
