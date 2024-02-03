88fa48ec9be8ba0c550e72bf8856f39053a5bc99
Tamara Patterson <tamarabpatterson@gmail.com>
2024-02-03 15:17:08 -0800

    add active section context to projects

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,12 +1,26 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Project from "./project";
+import { useActiveSectionContext } from "@/context/active-section-context";
+import { useInView } from "react-intersection-observer";
 
 
 export default function Projects() {
+    const { setActiveSection } = useActiveSectionContext();
+    const { ref, inView } = useInView();
+
+    // useEffect to keep external state of UI position with navigation
+    useEffect(() => {
+        if (inView) {
+            setActiveSection("Projects");
+        }        
+    }, [inView, setActiveSection])
+
     return (
-        <section id="projects" className="scroll-mt-28">
+        <section ref={ref} id="projects" className="scroll-mt-28">
             <SectionHeading>My projects</SectionHeading>
             <div>
                 {
