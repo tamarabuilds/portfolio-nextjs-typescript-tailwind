43a7cf690b1b76149f234fec2252de41a5da0f18
Tamara Patterson <tamarabpatterson@gmail.com>
2024-02-19 17:06:42 -0800

    add skills id

--- components/skills.tsx
+++ components/skills.tsx
@@ -26,7 +26,7 @@ export default function Skills() {
 
 
     return (
-        <section ref={ref} className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40">
+        <section id="skills" ref={ref} className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40">
             <SectionHeading> My Skills</SectionHeading>
             <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
                 {skillsData.map((skill, index) => (
