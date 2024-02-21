d6d594169ebbe4d64bd59f0120f7b04ac8def3b4
Tamara Patterson <tamarabpatterson@gmail.com>
2024-02-20 16:09:50 -0800

    update skills component

--- components/skills.tsx
+++ components/skills.tsx
@@ -4,7 +4,7 @@ import { skillsData } from "@/lib/data";
 
 export default function Skills() {
     return (
-        <section className="mb-28 max-w-[53rem] scroll-mt 28">
+        <section className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40">
             <SectionHeading> My Skills</SectionHeading>
             <ul>
                 {skillsData.map((skill, index) => (
