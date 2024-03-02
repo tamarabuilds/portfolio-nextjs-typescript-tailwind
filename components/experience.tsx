032448a3987ed7eb0810b3fdbfc61b87a75fb22c
Tamara Patterson <tamarabpatterson@gmail.com>
2024-03-02 14:32:23 -0800

    add to experience section

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from "react";
 import SectionHeading from "./section-heading";
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
@@ -5,19 +7,20 @@ import 'react-vertical-timeline-component/style.min.css';
 import { experiencesData } from "@/lib/data";
 
 export default function Experience() {
-    return <section id='experience'>
-        <SectionHeading>My experience</SectionHeading>
-        <VerticalTimeline>
-            {experiencesData.map((item, index) => (
-                <React.Fragment key={index}>
-                    <VerticalTimelineElement>
-                        <h3>{item.title}</h3>
-                        <p>{item.location}</p>
-                        <p>{item.description}</p>
-                    </VerticalTimelineElement>
-                </React.Fragment>
-            ))
-            }
-        </VerticalTimeline>
-    </section>
+    return (
+        <section id='experience'>
+            <SectionHeading>My experience</SectionHeading>
+            <VerticalTimeline>
+                {experiencesData.map((item, index) => (
+                    <React.Fragment key={index}>
+                        <VerticalTimelineElement>
+                            <h3>{item.title}</h3>
+                            <p>{item.location}</p>
+                            <p>{item.description}</p>
+                        </VerticalTimelineElement>
+                    </React.Fragment>
+                ))}
+            </VerticalTimeline>
+        </section>
+    );
 }
