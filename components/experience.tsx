0fe4adfeac7feb98c8a64bfc98cc155928fbeadf
Tamara Patterson <tamarabpatterson@gmail.com>
2024-03-04 13:53:07 -0800

    update skills component

--- components/experience.tsx
+++ components/experience.tsx
@@ -10,7 +10,7 @@ export default function Experience() {
     return (
         <section id='experience'>
             <SectionHeading>My experience</SectionHeading>
-            <VerticalTimeline>
+            <VerticalTimeline lineColor="">
                 {experiencesData.map((item, index) => (
                     <React.Fragment key={index}>
                         <VerticalTimelineElement
@@ -22,10 +22,19 @@ export default function Experience() {
                                 textAlign: 'left',
                                 padding: '1.3rem 2rem',
                             }}
+                            contentArrowStyle={{
+                                borderRight: '0.4rem solid #9ca3af'
+                            }}
+                            date={item.date}
+                            icon={item.icon}
+                            iconStyle={{
+                                 background: 'white',
+                                 fontSize: '1.5rem',
+                            }}
                         >
-                            <h3>{item.title}</h3>
-                            <p>{item.location}</p>
-                            <p>{item.description}</p>
+                            <h3 className="font-semibold capitalize">{item.title}</h3>
+                            <p className="font-normal !mt-0">{item.location}</p>
+                            <p className="!mt-1 !font-normal text-gray-700">{item.description}</p>
                         </VerticalTimelineElement>
                     </React.Fragment>
                 ))}
